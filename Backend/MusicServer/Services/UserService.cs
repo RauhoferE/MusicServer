c8fd37f27f8204d9133017b10c60b2b67f915e47
Emre Rauhofer <emre.rauhofer@gmail.com>
2023-10-22 13:45:22 +0200

    test

--- Backend/MusicServer/Services/UserService.cs
+++ Backend/MusicServer/Services/UserService.cs
@@ -253,7 +253,6 @@ namespace MusicServer.Services
         
         public async Task<AllFollowedEntitiesResponse> GetAllFollowedUsersArtistsPlaylistsFavorites(string filter, string searchTerm)
         {
-            //test
             filter = filter ?? string.Empty;
             searchTerm = searchTerm ?? string.Empty;
             searchTerm = searchTerm.ToLower();
