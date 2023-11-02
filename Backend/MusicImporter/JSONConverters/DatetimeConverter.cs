fa819f1beb009ce76952071ea8669645f8e2605c
Emre Rauhofer <emre.rauhofer@gmail.com>
2023-11-02 19:54:38 +0100

    Reworked datetime converter in importer

---
+++ Backend/MusicImporter/JSONConverters/DatetimeConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MusicImporter.JSONConverters
+{
+    internal class DatetimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.GetString() == null)
+            {
+                throw new Exception("No string found to convert to JSON");
+            }
+
+            if (string.IsNullOrEmpty(reader.GetString()))
+            {
+                return DateTime.Now;
+            }
+
+            try
+            {
+                return DateTime.Parse(reader.GetString());
+            }
+            catch (Exception)
+            {
+                return new DateTime(Convert.ToInt32(reader.GetString()), 1, 1);
+            }
+            
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
--- Backend/MusicImporter/JsonObjects/MusicBrainzRelease.cs
+++ Backend/MusicImporter/JsonObjects/MusicBrainzRelease.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicImporter.JSONConverters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace MusicImporter.JsonObjects
     {
         public string Id { get; set; }
 
+        [JsonConverter(typeof(DatetimeConverter))]
         public DateTime Date { get; set; }
 
         [JsonPropertyName("release-group")]
